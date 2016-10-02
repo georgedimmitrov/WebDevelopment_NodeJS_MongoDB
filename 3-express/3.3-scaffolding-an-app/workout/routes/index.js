34da336abe1102d5d6f5ab8e783867147900fa2b
georgi dimitrov <georgedimmitrov@gmail.com>
2016-10-02 16:30:55 +0300

    handlebars templating

---
+++ 3-express/3.3-scaffolding-an-app/workout/routes/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('./../lib/passport');
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
+/* GET home page. */
+router.get('/', function(request, response, next) {
+  response.render('index', { title: 'Express' });
+});
+
+router.route('/login')
+	.get(function(request, response) {
+		response.render('login', { error: request.flash('error') });
+	})
+	.post(passport.authenticate('local', {
+		successRedirect: '/profile',
+		failureRedirect: '/login',
+		failureFlash: true
+	}));
+
+router.get('/logout', function(request, response) {
+	request.logout();
+
+	response.redirect('/');
+});
+
+router.get('/profile', ensureLoggedIn(), function(request, response) {
+	response.send('Hello ' + request.user.username);
+});
+
+router.get('/mustache', function(request, response) {
+	response.render('mustache', {
+		name: 'Andrew',
+		htmlString: 'This <strong>contains</strong> HTML tags',
+		isLoggedIn: true,
+		isAdmin: false,
+		names: [
+			'Andrew',
+			'Cindy',
+			'Brian'
+		],
+		emptyList: [],
+		user: {
+			firstName: 'Andrew',
+			lastName: 'ThePleb',
+			contacts: {
+				twitter: 'lolkewl',
+				email: 'andrew.thePleb@home.com'
+			}
+		}
+	});
+})
+
+module.exports = router;
